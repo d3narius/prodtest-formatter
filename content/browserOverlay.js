a2143aa8d8a9ecca75aaa28cdeca1f4bb145c83f
Jonathan Shiloh Tatagiri <jonathanshiloh@gmail.com>
2012-05-28 03:28:09 -0700

    v1.0 - first commit

---
+++ content/browserOverlay.js
@@ -0,0 +1,28 @@
+/**
+ * ProdtestFormatterChrome namespace.
+ */
+if ("undefined" == typeof(ProdtestFormatterChrome)) {
+  var ProdtestFormatterChrome = {};
+};
+
+ProdtestFormatterChrome.BrowserOverlay = {
+  /**
+   * opens a new browser window with the formatted prodtest results
+   */
+  openLink : function(aEvent) {
+    let stringBundle = document.getElementById("prodtestformatter-string-bundle");
+    let errMsg = stringBundle.getString("prodtestformatter.error.msg");
+    
+    var validLink = false;
+    if(gContextMenu.onLink == true) {
+	var resultLink = new String(gContextMenu.target);
+	if (resultLink.indexOf("results.xml") != -1) {
+ 	    window.open("http://wwu-wsl/cgi-bin/parse_prodtest_xml.py?url="+resultLink+"&tab=1");
+            validLink = true;
+        }
+    }
+    if (validLink == false) {
+	window.alert(errMsg);
+    }
+  }
+};
